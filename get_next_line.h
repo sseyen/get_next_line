d273a958083fe798884af55d9d002f1fb50771f3
Alex <krisstalexx@gmail.com>
2024-10-14 19:09:02 +0200

    fixed norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:19:03 by alisseye          #+#    #+#             */
-/*   Updated: 2024/10/14 18:58:56 by alisseye         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:08:44 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdio.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 8
+#  define BUFFER_SIZE 8
 # endif
 
 char	*get_next_line(int fd);
